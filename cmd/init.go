ff3b42c27ee0272c3e3cb386cd10075d33fe5162
Rob Wilkerson <rob@robwilkerson.org>
2021-12-01 14:16:54 -0500

    Think about what the init command will do

--- cmd/init.go
+++ cmd/init.go
@@ -23,14 +23,15 @@ import (
 
 // initCmd represents the init command
 var initCmd = &cobra.Command{
-	Use:   "init",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
+	Use:     "init <project name>",
+	Aliases: []string{"initialize", "setup", "config", "configure"},
+	Short:   "Initializes a project for monitoring in Site24x7",
+	Long: `Creates the scaffolding atop which monitors are built for a given
+project. Scaffolding includes:
 
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+  * Monitor group
+  * User alert group
+  * Notification profile`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("init called; authentication successful")
 	},
