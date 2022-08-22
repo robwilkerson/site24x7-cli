55633e18b5e1175e50156616bb73a9f44d0b03e2
Rob Wilkerson <rob@robwilkerson.org>
2022-08-22 17:00:09 -0400

    Actually exiting if no config changes were made

--- cmd/config.go
+++ cmd/config.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+	http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -55,6 +55,7 @@ at $HOME/<username>/.site24x7.yaml.`,
 
 			if strings.ToUpper(overwrite) != "Y" {
 				fmt.Println("No changes were made; exiting.")
+				os.Exit(0)
 			}
 		}
 
